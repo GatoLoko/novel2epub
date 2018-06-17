157d131f37e6537e7afadea10a15d2b4bc06ac64
Raúl Soriano <GatoLoko@gmail.com>
2018-06-17 22:25:09 +0200

    Updated chapter list for ancient, atg, bem, mga, tv y usaw

--- Novels/tv.py
+++ Novels/tv.py
@@ -27,7 +27,7 @@ from common import Volume
 volumes = {'1': Volume('1 - ',
                        1, 100),
            '2': Volume('2 - ',
-                       101, 167),
+                       101, 174),
            }
 
 origin = 'http://www.wuxiaworld.com/novel/tranxending-vision/'
