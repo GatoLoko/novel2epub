ad268c01415830ab3d26744f0500817df57bc452
Raúl Soriano <GatoLoko@gmail.com>
2018-08-29 19:29:00 +0200

    Updated chapter list for ancient, atg, mga, tv and usaw

--- Novels/tv.py
+++ Novels/tv.py
@@ -29,7 +29,7 @@ volumes = {'1': Volume('1 - ',
            '2': Volume('2 - ',
                        101, 200),
            '3': Volume('3 - ',
-                       201, 237),
+                       201, 238),
            }
 
 origin = 'http://www.wuxiaworld.com/novel/tranxending-vision/'
