a77045079db8275dad6f3f77ad2522018214e47d
Raúl Soriano <GatoLoko@gmail.com>
2018-12-13 23:43:02 +0100

    Updated chapter list for: mga, pbs, tlw and tmw

--- Novels/tle.py
+++ Novels/tle.py
@@ -33,7 +33,7 @@ volumes = {'1': Volume('1', 0, 100),
            '4': Volume('4', 301, 400),
            '5': Volume('5', 401, 500),
            '6': Volume('6', 501, 600),
-           '7': Volume('7', 601, 603),
+           '7': Volume('7', 601, 607),
            }
 
 origin = 'http://www.wuxiaworld.co/The-Lord-is-Empire/'
