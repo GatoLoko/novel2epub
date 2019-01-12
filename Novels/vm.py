5fda2ac03da4e7a804e45d4c0944d79bf6e2d67e
Raúl Soriano <GatoLoko@gmail.com>
2019-01-12 01:15:36 +0100

    Updated chapter list for: tle, tmw, tv, usaw and vm

--- Novels/vm.py
+++ Novels/vm.py
@@ -26,7 +26,7 @@ from common import Volume
 
 volumes = {'1': Volume('1', 1, 100),
            '2': Volume('2', 101, 200),
-           '3': Volume('3', 201, 258),
+           '3': Volume('3', 201, 259),
            #
            '4': Volume('4', 301, 400),
            '5': Volume('5', 401, 500),
