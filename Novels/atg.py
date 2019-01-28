8d120a4800ad424f3e325d42f1eda8823c75fe07
Raúl Soriano <GatoLoko@gmail.com>
2019-01-29 00:30:25 +0100

    Updated chapter list for: ancient, atg, cobi and csg

--- Novels/atg.py
+++ Novels/atg.py
@@ -49,7 +49,7 @@ volumes = {'1': Volume('1 - Red-Colored calamity',
            '11': Volume('11 - Profound God convention',
                         1101, 1200),
            '12': Volume('12 - Infatuation in dreams',
-                        1201, 1276),
+                        1201, 1279),
            'X': Volume('X - END',
                        0000, 0000)
            }
