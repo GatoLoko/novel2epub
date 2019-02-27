a8dccaeb791f1b1e2bd88cfebef0817a88d842f1
Raúl Soriano <GatoLoko@gmail.com>
2019-02-27 21:32:25 +0100

    Updated chapter list for: csg, ed, gdk, mga and pbs

--- Novels/mga.py
+++ Novels/mga.py
@@ -39,7 +39,7 @@ volumes = {'1': Volume('1 - Battle of dignity',
            '7': Volume('7 - Return of the discarded child',
                        2673, 3108),
            '8': Volume('8 - Battle of prophecy',
-                       3109, 3279),
+                       3109, 3281),
            'X': Volume('X - ',
                        9999, 9999)
            }
