05affc7b8564d638cf098a98bead5b19cb74ba24
Raúl Soriano <GatoLoko@gmail.com>
2019-03-11 00:09:39 +0100

    Updated chapter list for: todg, tv, usaw and vm

--- Novels/todg.py
+++ Novels/todg.py
@@ -28,7 +28,7 @@ volumes = {'1': Volume('1', 1, 100),
            '2': Volume('2', 101, 200),
            '3': Volume('3', 201, 300),
            '4': Volume('4', 301, 400),
-           '5': Volume('5', 401, 474)
+           '5': Volume('5', 401, 475)
            }
 
 origin = 'http://www.wuxiaworld.com/novel/tales-of-demons-and-gods/'
@@ -59,8 +59,8 @@ def genlist(start, end):
     chapterlist = []
     for i in range(start, end+1):
         url = origin + 'tdg-chapter-' + str(i)
-        if i >= 472:
-            url = origin + 'tdg-chapter-' + str(i+1)
+        # if i >= 472:
+        #     url = origin + 'tdg-chapter-' + str(i+1)
         chapterlist.append(url)
     return chapterlist
 
