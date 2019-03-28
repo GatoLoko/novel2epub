84325dd53197c3def5c0f40069c11ff0d230599b
Raúl Soriano <GatoLoko@gmail.com>
2019-03-28 18:44:17 +0100

    Updated chapter list for: mwiabc, rtw, ss and tle

--- Novels/tle.py
+++ Novels/tle.py
@@ -35,7 +35,7 @@ volumes = {'1': Volume('1', 0, 100),
            '6': Volume('6', 501, 600),
            '7': Volume('7', 601, 700),
            '8': Volume('8', 701, 800),
-           '9': Volume('9', 801, 823),
+           '9': Volume('9', 801, 825),
            }
 
 origin = 'http://www.wuxiaworld.co/The-Lord-is-Empire/'
