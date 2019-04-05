c7dadaf9fa3ebd4f0afdcf2cf453427d525922c7
Raúl Soriano <GatoLoko@gmail.com>
2019-04-05 23:46:20 +0200

    Updated chapter list for: mga, mwiabc, rtw and ss

--- Novels/mwiabc.py
+++ Novels/mwiabc.py
@@ -32,7 +32,7 @@ volumes = {'1': Volume('1', 1, 100),
            '4': Volume('4', 301, 400),
            '5': Volume('5', 401, 500),
            '6': Volume('6', 501, 600),
-           '7': Volume('7', 601, 664),
+           '7': Volume('7', 601, 665),
            }
 
 origin = 'http://www.wuxiaworld.co/My-Wife-is-a-Beautiful-CEO/'
