6a41d08b9ba480bfcd0830c2d21733ea566d9dc7
Raúl Soriano <GatoLoko@gmail.com>
2019-04-09 01:13:57 +0200

    Updated chapter list for: cobi, csg, pbs, sr and todg

--- Novels/todg.py
+++ Novels/todg.py
@@ -28,7 +28,7 @@ volumes = {'1': Volume('1', 1, 100),
            '2': Volume('2', 101, 200),
            '3': Volume('3', 201, 300),
            '4': Volume('4', 301, 400),
-           '5': Volume('5', 401, 475)
+           '5': Volume('5', 401, 476)
            }
 
 origin = 'http://www.wuxiaworld.com/novel/tales-of-demons-and-gods/'
