a59c0bfdf336959cf1de942dacf47540d52ba555
Raúl Soriano <GatoLoko@gmail.com>
2019-04-21 22:36:37 +0200

    Updated chapter list for: ancient, atg, cobi and gdk

--- Novels/gdk.py
+++ Novels/gdk.py
@@ -32,7 +32,7 @@ volumes = {'1': Volume('1', 0, 100),
            '4': Volume('4', 301, 400),
            '5': Volume('5', 401, 500),
            '6': Volume('6', 501, 600),
-           '7': Volume('7', 601, 663),
+           '7': Volume('7', 601, 664),
            }
 
 origin = 'http://www.wuxiaworld.co/Great-Demon-King/'
