745f45f5cf55be96f0673f976d1bc8cda21e53ff
Raúl Soriano <GatoLoko@gmail.com>
2019-04-24 22:33:06 +0200

    Updated chapter list for: tle, tv, usaw and vm

--- Novels/tv.py
+++ Novels/tv.py
@@ -34,7 +34,7 @@ volumes = {'1': Volume('1 - ',
            '4': Volume('4 - ',
                        301, 400),
            '5': Volume('5',
-                       401, 476),
+                       401, 478),
            }
 
 origin = 'http://www.wuxiaworld.com/novel/tranxending-vision/'
