a0234287b0afa5ed40070bed4403c745f01affac
Raúl Soriano <GatoLoko@gmail.com>
2019-06-17 21:50:42 +0200

    Updated chapter list for: rtw, sr, ss and tle

--- Novels/ss.py
+++ Novels/ss.py
@@ -58,7 +58,7 @@ volumes = {'1': Volume('1 - Awakening in the night', 1, 20),
            '29': Volume('29', 1260, 1313),
            '30': Volume('30', 1314, 1338),
            '31': Volume('31', 1339, 1400),
-           '32': Volume('32', 1401, 1460),
+           '32': Volume('32', 1401, 1469),
            # Last chapter for this story is 1497
            }
 
