908d7d049bf05357ca70a219cc10970a7667f42c
Raúl Soriano <GatoLoko@gmail.com>
2019-07-10 21:32:51 +0200

    Updated chapter list for: mwiabc, pbs, rtw, sr and tle

--- Novels/rtw.py
+++ Novels/rtw.py
@@ -31,7 +31,7 @@ volumes = {'1': Volume('1', 1, 88),
            '3': Volume('3', 97, 481),
            '4': Volume('4', 482, 825),
            '5': Volume('5', 826, 1218),
-           '6': Volume('6', 1219, 1487),
+           '6': Volume('6', 1219, 1489),
            # This novel ends with chapter 1498
            }
 
