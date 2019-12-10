f8584844ca17f80aad42392fb7a421780f1c0821
Raúl Soriano <GatoLoko@gmail.com>
2019-12-10 22:58:18 +0100

    Updated chapter list for: gdk, mga, mwiabc and pbs

--- Novels/mga.py
+++ Novels/mga.py
@@ -41,7 +41,7 @@ volumes = {'1': Volume('1 - Battle of dignity',
            '8': Volume('8 - Battle of prophecy',
                        3109, 3764),
            '9': Volume('9 - Birth of Asura',
-                       3765, 3849)
+                       3765, 3855)
            # This novel ends with chapter 3934, vol9
            }
 
