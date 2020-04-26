472dc3fd3d4f56c42b2cf2751352b17280a8fb30
Raúl Soriano <GatoLoko@gmail.com>
2020-04-26 23:39:45 +0200

    Updated chapter list for: ed, mansion, mga and mwiabc

--- Novels/mga.py
+++ Novels/mga.py
@@ -41,8 +41,8 @@ volumes = {'1': Volume('1 - Battle of dignity',
            '8': Volume('8 - Battle of prophecy',
                        3109, 3764),
            '9': Volume('9 - Birth of Asura',
-                       3765, 4117)
-           # As of Jan 2020, this novel has 4139 chapters and isn't finished
+                       3765, 4128)
+           # As of Apr 2020, this novel has 4244 chapters and isn't finished
            }
 
 origin = 'http://www.wuxiaworld.com/novel/martial-god-asura/'
